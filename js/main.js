39dd6e1dd67cfb9dc07ce0fffae7abd9247bfad7
farazpc3 <farazpc3@gmail.com>
2025-07-25 13:45:28 +0330

    added js file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Numbers
+
+const myNumber = 42;
+
+const myFloat = 42.054;
+
+const myString ='42.112ae7';
+
+console.log(Number(myString) === myNumber);
+console.log(Number.parseFloat(myString));
